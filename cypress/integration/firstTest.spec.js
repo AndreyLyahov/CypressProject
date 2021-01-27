1d3a5ed34e4e6684183e80f6b0aba2684a392309
Andrii Liakhov <aliak@softserveinc.com>
2021-01-27 21:18:20 +0200

    lecture about cypress

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,36 @@
+// describe('My First Test', () => {
+  //  it('Does not do much!', () => {
+    //  expect(true).to.equal(true)
+    //})
+  //}) 
+
+
+it('By ID', ()=>{
+
+    cy.visit("https://www.facebook.com/")
+    cy.get('#email')
+});
+
+it('By Class', ()=>{
+
+    cy.visit("https://docs.cypress.io/api/commands/and.html#Syntax")
+    cy.get('.ds-input')
+});
+
+it('By Tag', ()=>{
+
+    cy.visit("https://docs.cypress.io/api/commands/and.html#Syntax")
+    cy.get('nav')
+});
+
+it('By Tag Value', ()=>{
+
+    cy.visit("https://www.facebook.com/")
+    cy.get('[name="pass"]')
+});
+
+it('Using Get with Find and Eq', ()=>{
+
+    cy.visit("https://next.privat24.ua/deposit")
+    cy.get('tbody').find('td').find('div').find('button').eq('0').click()
+});
