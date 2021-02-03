1052d5c627523692ffede2e3de733d35e9f099d5
ANDREYLYAHOV <dneprdiagnost@gmail.com>
2021-02-03 21:52:54 +0200

    02032021 commit

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,1 +1,19 @@
 /// <reference types="Cypress" />
+
+it('', () => {
+
+    cy.visit('https://docs.cypress.io/guides/getting-started/installing-cypress.html#Opening-Cypress')
+    cy.contains('Getting Started');
+})
+
+it('', () => {
+
+    cy.visit('https://docs.cypress.io/guides/getting-started/installing-cypress.html#Opening-Cypress')
+    cy.contains('strong','GETTING Started');
+})
+
+it.only('', () => {
+
+    cy.visit('https://docs.cypress.io/guides/getting-started/installing-cypress.html#Opening-Cypress')
+    cy.contains('GETTING Started', {matchCase: false});
+})
